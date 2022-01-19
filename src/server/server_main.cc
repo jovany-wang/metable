ad34373e04668e8c1036573d35b646a3121e9377
参考答案开心否 <46703189+wp19991@users.noreply.github.com>
2022-01-19 13:43:12 +0800

    Integrate spdlogs (#11)

--- src/server/server_main.cc
+++ src/server/server_main.cc
@@ -1,6 +1,12 @@
 #include <iostream>
+#include <fstream>
+#include "common/logging.h"
 
-int main(int argc, char** argv) {
+int main(int argc, char **argv) {
+    metable::MetableLog::StartMetableLog("log/test.log",
+                                         metable::MetableLogLevel::RLL_INFO, 10, 3);
     std::cout << "Metable Loading." << std::endl;
+    metable::MetableLog::ShutDownMetableLog();
+    std::fstream file("log/test.log");
     return 0;
 }
