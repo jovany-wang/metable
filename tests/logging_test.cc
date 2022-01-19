4b6934baecd532c466ee8013d384027dbaf0ab8c
wp <983214439@qq.com>
2022-01-19 13:33:44 +0800

    Integrate spdlogs,fix bug

--- tests/logging_test.cc
+++ tests/logging_test.cc
@@ -40,7 +40,7 @@ TEST(LoggingTest, TestLogLevel) {
 // Here tests trivial things for the logging
 //TEST(LoggingTest, TestLogTrivial) {
 //    metable::NodeID node_id;
-//    RAFTCPP_LOG(RLL_DEBUG) << node_id;
+//    METABLE_LOG(RLL_DEBUG) << node_id;
 //}
 
 int main(int argc, char **argv) {
