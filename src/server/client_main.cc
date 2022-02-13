067842c3a39f9ff7a111c86518db294e573a6967
Qing Wang <kingchin1218@gmail.com>
2022-02-13 22:08:54 +0800

    Add metable client lib

--- src/server/client_main.cc
+++ src/server/client_main.cc
@@ -4,61 +4,7 @@
 
 #include "src/protobuf/rpc.grpc.pb.h"
 #include "src/common/constants.h"
-
-#include "grpcpp/grpcpp.h"
-#include "grpcpp/health_check_service_interface.h"
-
-using grpc::Server;
-using grpc::ServerBuilder;
-using grpc::ServerContext;
-using grpc::Status;
-
-#include "src/protobuf/rpc.grpc.pb.h"
-#include <grpcpp/grpcpp.h>
-
-using grpc::Channel;
-using grpc::ClientContext;
-using grpc::Status;
-
-namespace metable {
-
-class MetableClient {
-public:
-  MetableClient(std::shared_ptr<Channel> channel)
-      : stub_(metable::rpc::Metable::NewStub(channel)) {}
-
-  // Assembles the client's payload, sends it and presents the response back
-  // from the server.
-  bool CheckVersion(const std::string &user) {
-    // Data we are sending to the server.
-    rpc::CheckVersionRequest request;
-    request.set_version_str(VERSION_STR);
-
-    // Container for the data we expect from the server.
-    rpc::CheckVersionReply reply;
-
-    // Context for the client. It could be used to convey extra information to
-    // the server and/or tweak certain RPC behaviors.
-    ClientContext context;
-
-    // The actual RPC.
-    Status status = stub_->CheckVersion(&context, request, &reply);
-
-    // Act upon its status.
-    if (status.ok()) {
-      return reply.status() == rpc::CheckVersionStatus::OK;
-    } else {
-      std::cout << "Failed to RPC. status = " << status.error_message() << std::endl;
-      return false;
-    }
-  }
-
-private:
-  std::unique_ptr<metable::rpc::Metable::Stub> stub_;
-};
-
-} // namespace metable
-
+#include "src/server/metable_client.h"
 
 int main(int argc, char **argv) {
   // Instantiate the client. It requires a channel, out of which the actual RPCs
@@ -88,10 +34,10 @@ int main(int argc, char **argv) {
     target_str = "localhost:10001";
   }
 
-  metable::MetableClient greeter(
+  metable::MetableClient metable_client(
       grpc::CreateChannel(target_str, grpc::InsecureChannelCredentials()));
   std::string user("qingwang");
-  bool result = greeter.CheckVersion(user);
+  bool result = metable_client.CheckVersion(user);
   std::cout << "Version matched ==" << result << std::endl;
 
   return 0;
---
+++ src/server/metable_client.h
@@ -0,0 +1,40 @@
+#pragma once
+
+#include "common/logging.h"
+#include <fstream>
+#include <iostream>
+
+#include "src/protobuf/rpc.grpc.pb.h"
+#include "src/common/constants.h"
+
+#include "grpcpp/grpcpp.h"
+#include "grpcpp/health_check_service_interface.h"
+
+using grpc::Server;
+using grpc::ServerBuilder;
+using grpc::ServerContext;
+using grpc::Status;
+
+#include "src/protobuf/rpc.grpc.pb.h"
+#include <grpcpp/grpcpp.h>
+
+using grpc::Channel;
+using grpc::ClientContext;
+using grpc::Status;
+
+namespace metable {
+
+class MetableClient {
+public:
+  MetableClient(std::shared_ptr<Channel> channel)
+      : stub_(metable::rpc::Metable::NewStub(channel)) {}
+
+  // Assembles the client's payload, sends it and presents the response back
+  // from the server.
+  bool CheckVersion(const std::string &user);
+
+private:
+  std::unique_ptr<metable::rpc::Metable::Stub> stub_;
+};
+
+} // namespace metable
