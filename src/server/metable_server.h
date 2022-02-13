053767bd41914065587b865e94b1aeb7f56b9739
Qing Wang <kingchin1218@gmail.com>
2022-02-13 22:02:42 +0800

    Add metable_server_lib

---
+++ src/server/metable_server.h
@@ -0,0 +1,40 @@
+#pragma once
+
+#include "common/logging.h"
+#include <fstream>
+#include <iostream>
+
+#include "src/protobuf/rpc.grpc.pb.h"
+#include "src/common/constants.h"
+
+#include "grpcpp/grpcpp.h"
+#include "grpcpp/health_check_service_interface.h"
+
+using grpc::Server;
+using grpc::ServerBuilder;
+using grpc::ServerContext;
+using grpc::Status;
+
+namespace metable {
+
+/// RPC service of Metable.
+class MetableServiceImpl : public rpc::Metable::Service {
+
+public:
+  virtual grpc::Status CheckVersion(::grpc::ServerContext *context,
+                                const rpc::CheckVersionRequest *request,
+                                rpc::CheckVersionReply *reply) override;
+
+};
+
+/// The server of Metable.
+class MetableServer final {
+
+public:
+    void Loop();
+
+private:
+
+};
+
+} // namespace metable
--- src/server/server_main.cc
+++ src/server/server_main.cc
@@ -2,36 +2,9 @@
 #include <fstream>
 #include <iostream>
 
-#include "src/protobuf/rpc.grpc.pb.h"
-#include "src/common/constants.h"
+#include "metable_server.h"
 
-#include "grpcpp/grpcpp.h"
-#include "grpcpp/health_check_service_interface.h"
-
-using grpc::Server;
-using grpc::ServerBuilder;
-using grpc::ServerContext;
-using grpc::Status;
-
-namespace metable {
-
-class MetableServiceImpl : public rpc::Metable::Service {
-
-public:
-  virtual grpc::Status CheckVersion(::grpc::ServerContext *context,
-                                const rpc::CheckVersionRequest *request,
-                                rpc::CheckVersionReply *reply) override {
-    std::cout << "Received version str = " << request->version_str();
-    if (VERSION_STR == request->version_str()) {
-      reply->set_status(rpc::CheckVersionStatus::OK);
-    } else {
-      reply->set_status(rpc::CheckVersionStatus::DISMATCHED);
-    }
-    return grpc::Status::OK;
-  }
-};
-
-} // namespace metable
+using namespace metable;
 
 int main(int argc, char **argv) {
   metable::MetableLog::StartMetableLog(
@@ -40,28 +13,8 @@ int main(int argc, char **argv) {
   metable::MetableLog::ShutDownMetableLog();
   std::fstream file("log/test.log");
 
-  {
-    /// For grpc server
-    std::string server_address("0.0.0.0:10001");
-    metable::MetableServiceImpl service;
-
-    grpc::EnableDefaultHealthCheckService(true);
-    // grpc::reflection::InitProtoReflectionServerBuilderPlugin();
-
-    ServerBuilder builder;
-    // Listen on the given address without any authentication mechanism.
-    builder.AddListeningPort(server_address, grpc::InsecureServerCredentials());
-    // Register "service" as the instance through which we'll communicate with
-    // clients. In this case it corresponds to an *synchronous* service.
-    builder.RegisterService(&service);
-    // Finally assemble the server.
-    std::unique_ptr<Server> server(builder.BuildAndStart());
-    std::cout << "Server listening on " << server_address << std::endl;
-
-    // Wait for the server to shutdown. Note that some other thread must be
-    // responsible for shutting down the server for this call to ever return.
-    server->Wait();
-  }
+  MetableServer metable_server;
+  metable_server.Loop();
 
   return 0;
 }
