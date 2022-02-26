926fa1ca9187556d25e2dce3f8147e7023054441
参考答案开心否 <46703189+wp19991@users.noreply.github.com>
2022-02-26 22:43:07 +0800

    Update greensky_skiplist_map.cc

--- src/skiplist_map/greensky_skiplist_map.cc
+++ src/skiplist_map/greensky_skiplist_map.cc
@@ -46,15 +46,21 @@ bool GreenSkySkiplistMap<KeyType, ValueType>::Contains(const KeyType &key) {
   return it != data_.end();
 }
 
-// The temporary function in "greensky_ skiplist_ map.cc" will resolve the link error. You do not need to call this function because it is global.
-// When the compiler encounters a greensky skiplistmap object declaration of a specific type, such as int, int, it must be able to access the template implementation source.
-// Otherwise, it will not know how to construct the greensky skiplistmap member function.
-// Moreover, if you put the implementation in the source file (greensky_skiplist_map.cc) and make it a separate part of the project, the compiler is trying to compile Mian CPP source file will not be found when
-// That is, it is not enough to just #include "greensky_skiplist_map.h", which only tells the compiler how to allocate object data and how to build calls to member functions, not how to build member functions.
-// At the same time, the compiler will not complain. It will assume that these functions are provided elsewhere and let the linker find them.
-// Therefore, when you need a link, you will get "unresolved references" to any class member function that is not defined as inline in the class definition.
-void TemporaryFunction (){
-  GreenSkySkiplistMap<int,int> TempObj;
-}
+// The temporary function in "greensky_ skiplist_ map.cc" will resolve the link
+// error. You do not need to call this function because it is global. When the
+// compiler encounters a greensky skiplistmap object declaration of a specific
+// type, such as int, int, it must be able to access the template implementation
+// source. Otherwise, it will not know how to construct the greensky skiplistmap
+// member function. Moreover, if you put the implementation in the source file
+// (greensky_skiplist_map.cc) and make it a separate part of the project, the
+// compiler is trying to compile Mian CPP source file will not be found when
+// That is, it is not enough to just #include "greensky_skiplist_map.h", which
+// only tells the compiler how to allocate object data and how to build calls to
+// member functions, not how to build member functions. At the same time, the
+// compiler will not complain. It will assume that these functions are provided
+// elsewhere and let the linker find them. Therefore, when you need a link, you
+// will get "unresolved references" to any class member function that is not
+// defined as inline in the class definition.
+void TemporaryFunction() { GreenSkySkiplistMap<int, int> TempObj; }
 
 } // namespace metable
