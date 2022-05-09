f07c9e724e87892c5217a7cb8f5ff4d928e38232
meijie <meijie@laiye.com>
2022-05-09 22:15:33 +0800

    implement sql feature

---
+++ tests/sql_test.cc
@@ -0,0 +1,12 @@
+#include "gtest/gtest.h"
+#include "sql/sql_parser.h"
+
+TEST(SqlTest, TestQuery) {
+    SqlParser parser;
+    SqlSelect select = parser.parser("select * from test");
+}
+
+int main(int argc, char **argv) {
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
