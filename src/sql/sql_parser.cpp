bdfaa5d693914378cc71003213090361fcfc7c17
meijie <meijie@laiye.com>
2022-05-09 22:12:22 +0800

    implement sql feature

--- src/sql/sql_parser.cpp
+++ src/sql/sql_parser.cpp
@@ -1,16 +1,35 @@
 #include "src/sql/sql_parser.h"
-#include "antlr4-runtime.h"
+
 #include "SqlGrammarParser.h"
 #include "SqlLexer.h"
+#include "antlr4-runtime.h"
+#include "string"
 
 using namespace antlr4;
 
-Stmt SqlParser::parse(std::string statement) 
-{
+SqlSelect SqlParser::parse(std::string statement) {
     ANTLRInputStream input(statement);
     SqlLexer lexer(&input);
     CommonTokenStream tokens(&lexer);
     SqlGrammarParser parser(&tokens);
     tree::ParseTree *tree = parser.statement();
-    
+    MetableSqlVisitor visitor;
+    return visitor.visit(tree).as<>(SqlSelect);
 }
+
+SqlSelect::SqlSelect(std::vector<SqlExpression> selectList, SqlFrom from) {
+    this->from = from;
+    this->selectList = selectList;
+}
+
+SqlFrom::SqlFrom(std::string tableIdentify) { this->tableIdentify = tableIdentify; }
+
+SqlSelect MetableSqlVisitor::withSqlSelect(
+    SqlGrammarParser::SelectClauseContext *selectContext,
+    SqlGrammarParser::FromClauseContext *fromClause,
+    SqlGrammarParser::WhereClauseContext *whereClause) {
+    SqlSelect select = {visitSelectClause(selectContext).as<std::vector<SqlExpression>>(),
+                        visitFromClause(fromClause).as<SqlFrom>()};
+}
+
+SqlExpression::SqlExpression(ConstantValue constant) { this->constant = constant; }
--- src/sql/sql_parser.h
+++ src/sql/sql_parser.h
@@ -1,18 +1,91 @@
 #pragma once
 
+#include <iostream>
 #include <string>
 
-#include "SqlGrammarVisitor.h"
+#include "SqlGrammarBaseVisitor.h"
 
-class Stmt {};
+enum ConstantValue {
+    ASTERISK,
+};
 
-class QueryStmt {};
+class SqlNode {};
+
+// only support * expression
+class SqlExpression : public SqlNode {
+private:
+    ConstantValue constant;
+
+public:
+    SqlExpression(ConstantValue constant);
+};
+
+class SqlNamedExpression : public SqlExpression {
+private:
+    std::string alias;
+};
+class SqlFrom : public SqlNode {
+private:
+    std::string tableIdentify;
+
+public:
+    SqlFrom(std::string tableIdentify);
+};
+
+// select * from table;
+class SqlSelect : public SqlNode {
+private:
+    std::vector<SqlExpression> selectList;
+    SqlFrom from;
+
+public:
+    SqlSelect(std::vector<SqlExpression> selectList, SqlFrom from);
+};
 
 class SqlParser {
 public:
-    Stmt parse(std::string statement);
+    SqlSelect parser(std::string sql);
 };
 
-class MetableSqlVisitor : public SqlGrammarVisitor {
-    
+class MetableSqlVisitor : public SqlGrammarBaseVisitor {
+private:
+    SqlSelect withSqlSelect(SqlGrammarParser::SelectClauseContext *selectContext,
+                            SqlGrammarParser::FromClauseContext *fromContext,
+                            SqlGrammarParser::WhereClauseContext *whereContext);
+
+public:
+    virtual antlrcpp::Any visitQueryStatement(
+        SqlGrammarParser::QueryStatementContext *ctx) override {
+        return withSqlSelect(ctx->selectClause(), ctx->fromClause(), ctx->whereClause());
+    }
+
+    virtual antlrcpp::Any visitSelectClause(
+        SqlGrammarParser::SelectClauseContext *ctx) override {
+        std::vector<SqlGrammarParser::NamedExpressionContext *> namedExpressCtxVec =
+            ctx->nameExpressionSeq()->namedExpression();
+        std::vector<SqlExpression> expressVector = {};
+        for (auto namedExpress : namedExpressCtxVec) {
+            expressVector.push_back(visitNamedExpression(namedExpress).as<SqlExpression>());
+        }
+        return expressVector;
+    }
+
+    virtual antlrcpp::Any visitNamedExpression(
+        SqlGrammarParser::NamedExpressionContext *ctx) override {
+        // todo skip alias now.
+        return visit(ctx->expression());
+    }
+
+    virtual antlrcpp::Any visitLogicalPredicate(
+        SqlGrammarParser::LogicalPredicateContext *ctx) override {
+        if (ctx->predicate() == nullptr) {
+            return visit(ctx->valueExpression());
+        }
+        // todo
+        return antlrcpp::Any();
+    }
+
+    virtual antlrcpp::Any visitStar(SqlGrammarParser::StarContext *ctx) override {
+        return SqlExpression(ASTERISK);
+    }
 };
